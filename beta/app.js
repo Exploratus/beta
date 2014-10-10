5f4af2a7609a615d50af7fc34d0cd441a283c3ad
amizrahi <amizrahi@192.168.1.181>
2014-10-10 13:52:18 -0400

    frrfr

--- beta/app.js
+++ beta/app.js
@@ -22,7 +22,7 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
+// development only ggfg
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
